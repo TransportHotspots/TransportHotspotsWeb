6d14cc8c0bbbd36324a86206bd8315f4d5bebd8e
syleung <lemonzpam@hotmail.com>
2016-07-31 14:54:36 +1000

    Added team logo, updated styling and updated mock dataset.

--- src/main/webapp/resources/js/transporthotspots-controllers.js
+++ src/main/webapp/resources/js/transporthotspots-controllers.js
@@ -4,22 +4,30 @@ appControllers.controller('searchController', [
     '$scope',
     function($scope) {
         var vm = this;
-        vm.currentUserRating = undefined;
-
         $scope.fromLocations = {
-            '0': 'North Sydney',
-            '1': 'b',
-            '2': 'c',
-            '3': 'd',
-            '4': 'e'
+            '0': 'Bondi Junction',
+            '1': 'Circular Quay',
+            '2': 'Central',
+            '3': 'Martin Place',
+            '4': 'Museum',
+            '5': 'Olympic Park',
+            '6': 'Redfern',
+            '7': 'St James',
+            '8': 'Town Hall',
+            '9': 'Wynyard'
         };
 
         $scope.toLocations = {
-            '0': 'North Sydney',
-            '1': 'b',
-            '2': 'c',
-            '3': 'd',
-            '4': 'e'
+            '0': 'Bondi Junction',
+            '1': 'Circular Quay',
+            '2': 'Central',
+            '3': 'Martin Place',
+            '4': 'Museum',
+            '5': 'Olympic Park',
+            '6': 'Redfern',
+            '7': 'St James',
+            '8': 'Town Hall',
+            '9': 'Wynyard'
         };
 
         $scope.enabledVotingLocations = {};
@@ -29,29 +37,60 @@ appControllers.controller('searchController', [
             var selectedToLocation = $scope.selectedToLocation;
 
             $scope.routes = {
-                '0': {
-                    'name': 'Wynyard',
+                '1': {
+                    'stationId': '5',
+                    'name': 'Olympic Park',
                     'fromLocation': true,
                     'toLocation': true,
-                    'isTransit': false,
-                    'rating': 2,
-                    'enableVoting': true,
-                    'currentUserRating': undefined
-                },
-                '1': {
-                    'name': 'Town Hall',
-                    'fromLocation': false,
-                    'toLocation': false,
-                    'isTransit': true,
                     'rating': 1,
                     'enableVoting': true,
                     'currentUserRating': undefined
                 },
                 '2': {
+                    'stationId': '6',
+                    'name': 'Redfern',
+                    'fromLocation': false,
+                    'toLocation': false,
+                    'transit': {
+                        'transitOrder': 1,
+                        'transitTo': 'Town Hall'
+                    },
+                    'transitTo': 'Town Hall',
+                    'rating': 3,
+                    'enableVoting': true,
+                    'currentUserRating': undefined
+                },
+                '3': {
+                    'stationId': '2',
                     'name': 'Central',
                     'fromLocation': false,
+                    'toLocation': false,
+                    'transit': {
+                        'transitOrder': 1,
+                        'transitTo': 'Town Hall'
+                    },
+                    'rating': 1,
+                    'enableVoting': true,
+                    'currentUserRating': undefined
+                },
+                '4': {
+                    'stationId': '8',
+                    'name': 'Town Hall',
+                    'fromLocation': false,
+                    'toLocation': false,
+                    'transit': {
+                        'transitOrder': 2,
+                        'transitTo': 'Bondi Junction'
+                    },
+                    'rating': 2,
+                    'enableVoting': true,
+                    'currentUserRating': undefined
+                },
+                '5': {
+                    'stationId': '0',
+                    'name': 'Bondi Junction',
+                    'fromLocation': false,
                     'toLocation': true,
-                    'isTransit': false,
                     'rating': 3,
                     'enableVoting': true,
                     'currentUserRating': undefined
